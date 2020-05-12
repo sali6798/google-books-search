04c59372390bd9363f645757d6f831eb8fd8bb12
sali6798 <shaidee@alingcastre.net>
2020-05-12 08:34:58 -0700

    server setup complete

---
+++ server.js
@@ -0,0 +1,35 @@
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// Routes
+// =============================================================
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+// Start the API server
+app.listen(PORT, function () {
+    console.log(`API Server now listening on PORT ${PORT}!`);
+});
