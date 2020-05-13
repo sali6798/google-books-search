6e857decd0e044225fcf428f69c03a8dd6a5d541
sali6798 <shaidee@alingcastre.net>
2020-05-12 22:24:02 -0700

    routes to api and Google books API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,16 @@
 import axios from "axios";
 
 export default {
-    
+    search: function (search) {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${search}&maxResults=40&key=AIzaSyDlcftYZPY_X1lyarWB-0AYX20zyRLeq7E`)
+    },
+    save: function (book) {
+        return axios.post("/api/books", book);
+    },
+    getBooks: function () {
+        return axios.get("/api/books");
+    },
+    removeBook: function (id) {
+        return axios.delete("/api/books/" + id)
+    }
 }
