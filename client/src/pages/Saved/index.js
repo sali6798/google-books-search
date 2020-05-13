e6e89cc0531cfcd1e2726d6fbd7460cc55265efc
sali6798 <shaidee@alingcastre.net>
2020-05-12 22:28:18 -0700

    functioning saved page + styling

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../../utils/API";
+import BookContainer from "../../components/BookContainer";
+import "./style.css"
 
 function Search() {
-    // return ();
+    const [savedBooks, setSavedBooks] = useState([]);
+
+    useEffect(() => {
+        loadBooks()
+    }, []);
+
+    const loadBooks = () => {
+        API.getBooks()
+            .then(res =>
+                setSavedBooks(res.data)
+            )
+            .catch(err => console.log(err));
+    };
+
+    const handleClick = book => {
+        API.removeBook(book._id)
+        .then(() => loadBooks())
+        .catch(err => console.log(err))
+    }
+
+    return (
+        <div className="Saved">
+            <h3>Saved Books</h3>
+            {savedBooks.map(book => <BookContainer key={book._id} {...book} handleClick={handleClick} />)}
+        </div>
+    );
 }
 
-export default Search;
+export default Search;  
