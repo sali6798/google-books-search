66abec9b499b443ce4e45c64233ce6ec5ff485fb
sali6798 <shaidee@alingcastre.net>
2020-05-12 23:04:40 -0700

    hidden API key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
     search: function (search) {
-        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${search}&maxResults=40&key=AIzaSyDlcftYZPY_X1lyarWB-0AYX20zyRLeq7E`)
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${search}&maxResults=40&key=${process.env.REACT_APP_API_KEY}`)
     },
     save: function (book) {
         return axios.post("/api/books", book);
