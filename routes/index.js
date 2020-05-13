285ff5ce95364023ae774254ac226c88b0a8a3e1
sali6798 <shaidee@alingcastre.net>
2020-05-12 23:40:03 -0700

    added route for google books search

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const axios = require("axios");
+require("dotenv").config();
 
 // API Routes
 router.use("/api/books", apiRoutes);
+
+router.get("/search/:search", (req, res) => {
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${req.params.search}&maxResults=40&key=${process.env.API_KEY}`)
+  .then(({ data }) => res.json(data))
+  .catch(err => console.log(err))
+})
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
