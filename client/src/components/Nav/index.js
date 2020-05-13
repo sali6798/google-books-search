c4b15f62090539d4493b06b9aada6ab6a806ec24
sali6798 <shaidee@alingcastre.net>
2020-05-12 23:10:05 -0700

    logo change

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,7 +6,7 @@ function Nav() {
         <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
             <nav className="uk-navbar-container" uk-navbar="true">
                 <div className="uk-navbar-left">
-                    <a href="/" className="uk-navbar-item uk-logo">Google Books</a>
+                    <a href="/" className="uk-navbar-item uk-logo">Not Google Books</a>
                     <ul className="uk-navbar-nav">
                         <li><a href="/">Search</a></li>
                         <li><a href="/saved">Saved</a></li>
