344a5b58efbad9be339e8172c9882b7e2525203c
sali6798 <shaidee@alingcastre.net>
2020-05-12 22:29:04 -0700

    created sticky navbar + styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./style.css";
 
 function Nav() {
-    // return();
+    return (
+        <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
+            <nav className="uk-navbar-container" uk-navbar="true">
+                <div className="uk-navbar-left">
+                    <a href="/" className="uk-navbar-item uk-logo">Google Books</a>
+                    <ul className="uk-navbar-nav">
+                        <li><a href="/">Search</a></li>
+                        <li><a href="/saved">Saved</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
 }
 
 export default Nav;
