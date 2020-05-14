3e25ae795233b179c9e35e27fa34f6c75d76aa70
sali6798 <shaidee@alingcastre.net>
2020-05-14 08:07:20 -0700

    loads saved books

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BookContainer from "../../components/BookContainer"
 import API from "../../utils/API"
 import "./style.css"
@@ -6,39 +6,59 @@ import "./style.css"
 function Search() {
     const [search, setSearch] = useState("");
     const [books, setBooks] = useState([]);
-    
+    const [savedBooks, setSavedBooks] = useState([]);
+
+    useEffect(() => {
+        loadBooks()
+    }, []);
+
+    const loadBooks = () => {
+        API.getBooks()
+            .then(res =>
+                setSavedBooks(res.data)
+            )
+            .catch(err => console.log(err));
+    };
+
     const handleInputChange = event => {
         setSearch(event.target.value);
     }
-    
+
     const handleSubmit = event => {
         event.preventDefault();
 
         API.search(search)
             .then(({ data }) => {
-                console.log(data);
                 setBooks(data.items);
             })
             .catch(err => console.log(err))
     }
 
-    const handleClick = book => {
-        console.log(book)
-        API.save({
-            title: book.title,
-            subtitle: book.subtitle,
-            authors: book.authors,
-            description: book.description,
-            image: book.image,
-            link: "https://www.google.com/books/edition/_/" + book.id
-        })
-        .then(newBook => console.log(newBook.data))
-
+    const handleClick = (book, action) => {
+        if (action === "Save") {
+            API.save({
+                title: book.title,
+                subtitle: book.subtitle,
+                authors: book.authors,
+                description: book.description,
+                image: book.image,
+                link: "https://www.google.com/books/edition/_/" + book.id
+            })
+                .then(newBook => {
+                    setSavedBooks(savedBooks.concat(newBook.data));
+                })
+        }
+        else {
+            const removeBook = savedBooks.find(savedBook => savedBook.link === book.link)
+            API.removeBook(removeBook._id)
+                .then(res => loadBooks())
+                .catch(err => console.log(err))
+        }
     }
 
     const displayBook = book => {
         const info = book.volumeInfo;
-        return <BookContainer 
+        return <BookContainer
             key={book.id}
             id={book.id}
             title={info.title}
@@ -47,16 +67,17 @@ function Search() {
             description={info.description}
             image={info.imageLinks ? info.imageLinks.thumbnail : null}
             link={"https://www.google.com/books/edition/_/" + book.id}
-            type="search" 
+            type="search"
+            savedBooks={savedBooks}
             handleClick={handleClick}
         />;
     }
 
     return (
         <div className="Search">
-            <form className="uk-search uk-search-default" onSubmit={handleSubmit}> 
+            <form className="uk-search uk-search-default" onSubmit={handleSubmit}>
                 <button className="uk-search-icon-flip" type="submit" uk-search-icon="true"></button>
-                <input className="uk-search-input" type="search" value={search} placeholder="Search For A Book" onChange={handleInputChange}/>
+                <input className="uk-search-input" type="search" value={search} placeholder="Search For A Book" onChange={handleInputChange} />
             </form>
             {books.map(book => displayBook(book))}
         </div>
