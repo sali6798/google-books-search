bcf890113d3fbc825dfae736cb2613ddf196ba70
sali6798 <shaidee@alingcastre.net>
2020-05-14 08:08:06 -0700

    mongo connect variables

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // Start the API server
 app.listen(PORT, function () {
