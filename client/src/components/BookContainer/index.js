82a3f9273259e3c52a5241b385d9de55049302f0
sali6798 <shaidee@alingcastre.net>
2020-05-14 08:06:56 -0700

    loads saved books

--- client/src/components/BookContainer/index.js
+++ client/src/components/BookContainer/index.js
@@ -2,6 +2,18 @@ import React from "react";
 import "./style.css"
 
 function BookContainer(props) {
+    const handleClick = event => {
+        return props.handleClick(props, event.target.name);
+    }
+
+    const buttonDisplay = () => {
+        if (props.type && !props.savedBooks.find(book => book.link === props.link)) {
+            return <button className="uk-button uk-button-default" name="Save" onClick={handleClick}>Save</button>;
+        }
+
+        return <button className="uk-button uk-button-default" name="Remove" onClick={handleClick}>Remove</button>;
+    }
+
     return (
         <div className="BookContainer">
             <div className="uk-card uk-card-default uk-card-body uk-width-1-1">
@@ -12,21 +24,28 @@ function BookContainer(props) {
                         <h5>Written By: {props.authors ? props.authors.join(", ") : "Unknown"}</h5>
                     </div>
                     <div className="uk-width-1-3 buttonContainer" uk-margin="true">
-                        <a className="uk-button uk-button-default" href={props.link} target="_blank" rel="noreferrer noopener">View</a>
-                        <button className="uk-button uk-button-default" onClick={() => props.handleClick(props)}>{props.type ? "Save" : "Remove"}</button>
+                        <a
+                            className="uk-button uk-button-default"
+                            href={props.link} target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            View
+                        </a>
+                        {buttonDisplay()}
                     </div>
-
                 </div>
                 <div uk-grid="true">
                     <div className="uk-width-1-6">
-                        <img src={props.image ? props.image : "https://via.placeholder.com/150"} alt={props.image ? props.title + " thumbnail" : "placeholder image C/O https://placeholder.com/"} />
+                        <img
+                            src={props.image ? props.image : "https://via.placeholder.com/150"}
+                            alt={props.image ? props.title + " thumbnail" : "placeholder image C/O https://placeholder.com/"}
+                        />
                     </div>
                     <div className="uk-width-5-6">
                         <p>{props.description}</p>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
